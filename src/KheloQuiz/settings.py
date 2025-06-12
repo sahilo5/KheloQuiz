b6b800835dc84c7bc5ff5a445495ba2af275cc0a
sahilo5 <sahildeshmukh54321@gmail.com>
2025-06-12 12:26:10 +0530

    Updated settings.py for deployment.

--- src/KheloQuiz/settings.py
+++ src/KheloQuiz/settings.py
@@ -81,14 +81,26 @@ WSGI_APPLICATION = 'KheloQuiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME", "KheloQuiz"),  
+#         "USER": os.getenv("DB_USER", "postgres"),   
+#         "PASSWORD": os.getenv("DB_PWD", "123"),  
+#         "HOST": os.getenv("DB_HOST", "localhost"), 
+#         "PORT": os.getenv("DB_PORT", "5432"), 
+#     }
+# }
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "KheloQuiz"),  
-        "USER": os.getenv("DB_USER", "postgres"),   
-        "PASSWORD": os.getenv("DB_PWD", "123"),  
-        "HOST": os.getenv("DB_HOST", "localhost"), 
-        "PORT": os.getenv("DB_PORT", "5432"), 
+        "NAME": "railway",  
+        "USER": "postgres",   
+        "PASSWORD":"sAWLPknpikTbLZbIOnjsjJtEiNYOsCKm",  
+        "HOST": "switchyard.proxy.rlwy.net", 
+        "PORT": "35615", 
     }
 }
 
