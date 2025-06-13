7d6165b86c205185c1fad694a0db9e64321b2142
sahilo5 <sahildeshmukh54321@gmail.com>
2025-06-13 10:45:30 +0530

    modified models.py

--- app/KheloQuiz/settings.py
+++ app/KheloQuiz/settings.py
@@ -93,11 +93,11 @@ if DB_LIVE in ["False",False]:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME", "KheloQuiz"),  
-            "USER": os.getenv("DB_USER", "postgres"),   
-            "PASSWORD": os.getenv("DB_PWD", "123"),  
-            "HOST": os.getenv("DB_HOST", "localhost"), 
-            "PORT": os.getenv("DB_PORT", "5432"), 
+            "NAME": "KheloQuiz",
+            "USER": "postgres", 
+            "PASSWORD": "123",
+            "HOST": "localhost",
+            "PORT": "5432"
         }
     }
 
