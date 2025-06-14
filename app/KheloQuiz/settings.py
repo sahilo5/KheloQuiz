951f41de4533e620b79f3ba2b354f8dd9208890a
sahilo5 <sahildeshmukh54321@gmail.com>
2025-06-14 17:52:03 +0530

    changes done

--- app/KheloQuiz/settings.py
+++ app/KheloQuiz/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fpr-@i6!j@8f9o5top*))l9dny%6t)8wuw0v1d$m^4@ws101&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','kheloquiz.up.railway.app']
 
